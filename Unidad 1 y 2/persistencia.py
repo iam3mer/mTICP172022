fda2f2917b4d1781d2809ee8f2d0af585392021d
iam3mer <jhstbarrera@utp.edu.co>
2021-05-17 09:04:20 -0500

    Unidad 1 y 2

---
+++ Unidad 1 y 2/persistencia.py
@@ -0,0 +1,14 @@
+valor = 10
+
+def suma(x, y):
+    valor = x + y
+    return valor
+
+
+'''
+print(suma(valor, 5)) #15
+print(valor) # 10
+'''
+
+valor = suma(valor, 5)
+print(valor) # 15
--- Unidad 1 y 2/ukumari.py
+++ Unidad 1 y 2/ukumari.py
@@ -10,36 +10,45 @@ def cargaGaseosa(amigo_1:dict=0,amigo_2:dict=0,amigo_3:dict=0,misDatos:dict={})-
     s2 = vaso2 > amigo_2['capacidad_vaso']
     s3 = vaso3 > amigo_3['capacidad_vaso']
 
+    contador = misDatos['contador'] + 1
+
     # Si a los tres se les riega devolver el nombre de los tres
     if s1 and s2 and s3:
-        contador = misDatos['contador'] + 1
         return {'contador': contador}, {1: amigo_1['nombre'], 2: amigo_2['nombre'], 3: amigo_3['nombre']}
     elif s1 and s2:
-        return amigo_1['nombre'], amigo_2['nombre']
+        return {'contador': contador}, {1: amigo_1['nombre'], 2: amigo_2['nombre']}
     elif s1 and s3:
-        return amigo_1['nombre'], amigo_3['nombre']
+        return {'contador': contador}, {1: amigo_1['nombre'], 3: amigo_3['nombre']}
     elif s2 and s3:
-        return amigo_2['nombre'], amigo_3['nombre']
+        return {'contador': contador}, {2: amigo_2['nombre'], 3: amigo_3['nombre']}
     elif s1:
-        return amigo_1['nombre']
+        return {'contador': contador}, {1: amigo_1['nombre']}
     elif s2:
-        return amigo_2['nombre']
+        return {'contador': contador}, {2: amigo_2['nombre']}
     elif s3:
-        return amigo_3['nombre']
+        return {'contador': contador}, {3: amigo_3['nombre']}
     else:
-        return None
+        return {'contador': contador}, None
 
 
+estado = {'contador': 0}
+
+'''
 vAmigo1={'nombre': 'Juan', 'capacidad_vaso': 15, 'capacidad_actual': 10}
 vAmigo2=dict(nombre='Esteban',capacidad_vaso=15,capacidad_actual=5.8)
 vAmigo3={'nombre':'Angelica','capacidad_vaso':15,'capacidad_actual': 12}
 # Angelica
+'''
 
 vAmigo1={'nombre': 'Juan', 'capacidad_vaso': 10, 'capacidad_actual': 8}
 vAmigo2=dict(nombre='Esteban',capacidad_vaso=10,capacidad_actual=3)
 vAmigo3={'nombre':'Angelica','capacidad_vaso':10,'capacidad_actual': 7.4}
 # (Juan, Angelica)
 
+estado, mensaje = cargaGaseosa(vAmigo1, vAmigo2, vAmigo3, estado)
+print(estado, mensaje)
+
+'''
 vAmigo1={'nombre': 'Juan', 'capacidad_vaso': 10, 'capacidad_actual': 8}
 vAmigo2=dict(nombre='Esteban',capacidad_vaso=10,capacidad_actual=7)
 vAmigo3={'nombre':'Angelica','capacidad_vaso':10,'capacidad_actual': 2}
@@ -49,23 +58,26 @@ vAmigo1={'nombre': 'Juan', 'capacidad_vaso': 10, 'capacidad_actual': 3}
 vAmigo2=dict(nombre='Esteban',capacidad_vaso=10,capacidad_actual=7.5)
 vAmigo3={'nombre':'Angelica','capacidad_vaso':10,'capacidad_actual': 6.8}
 # (Juan, Angelica)
+'''
 
 vAmigo1={'nombre': 'Juan', 'capacidad_vaso': 10, 'capacidad_actual': 3}
 vAmigo2=dict(nombre='Esteban',capacidad_vaso=10,capacidad_actual=4)
 vAmigo3={'nombre':'Angelica','capacidad_vaso':10,'capacidad_actual': 5}
-# (Juan, Angelica)
+# None
+
+estado, mensaje = cargaGaseosa(vAmigo1, vAmigo2, vAmigo3, estado)
+print(estado, mensaje)
 
 vAmigo1={'nombre': 'Juan', 'capacidad_vaso': 15, 'capacidad_actual': 12.4}
 vAmigo2=dict(nombre='Esteban',capacidad_vaso=15,capacidad_actual=14)
 vAmigo3={'nombre':'Angelica','capacidad_vaso':15,'capacidad_actual': 11}
-# (Juan, Esteban, Angelica)
+
+
+estado, mensaje = cargaGaseosa(vAmigo1, vAmigo2, vAmigo3, estado)
+print(estado, mensaje)
 
 #print(cargaGaseosa(vAmigo1, vAmigo2, vAmigo3))
 
-estado = {'contador': 0}
-estado, mensaje = cargaGaseosa(vAmigo1, vAmigo2, vAmigo3, estado)
-print(estado)
-estado, mensaje = cargaGaseosa(vAmigo1, vAmigo2, vAmigo3, estado)
-print(estado)
+
 
 
