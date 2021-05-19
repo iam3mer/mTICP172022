29a5e9df0804e867592fdac8e9be4d45d8ccaa8a
iam3mer <jhstbarrera@utp.edu.co>
2021-05-19 16:03:30 -0500

    conjuntos

---
+++ Unidad 3/conjuntos.py
@@ -0,0 +1,15 @@
+# Conjuntos
+
+conjuntoMutable = {234234,5345345,645456,345.354345, (456456465,34535745)}
+conjuntoMutable.add(645456)
+conjuntoMutable.add(3453)
+
+#print(conjuntoMutable)
+
+conjuntoMutable.pop()
+#print(conjuntoMutable)
+
+conjuntoMutable.remove(345.354345)
+print(conjuntoMutable)
+
+print(conjuntoMutable.union([3345345,345754,343543675,67,435,234234]))
