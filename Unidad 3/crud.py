cce9e5709e0c3c5b018e27d8bd719a9754014c2b
iam3mer <jhstbarrera@utp.edu.co>
2021-05-24 08:14:54 -0500

    Se añade al crud update y delete

--- Unidad 3/crud.py
+++ Unidad 3/crud.py
@@ -1,7 +1,7 @@
 # CRUD (Create, Read, Update, Delete)
 
 directorio = {
-    1: {
+    1254587: {
         'nombre': 'Jhonatan',
         'ciudad': 'Pereira',
         'profesion': 'Ingeniero de Sistemas y Computación',
@@ -12,7 +12,7 @@ directorio = {
             'frances': False
         }
     },
-    2: {
+    2548754: {
         'nombre': 'Octavio',
         'ciudad': 'Manizales',
         'profesion': 'Ecomista',
@@ -25,33 +25,59 @@ directorio = {
     }
 }
 
-def menu(opcion:int)->None:
-    print('----- CRUD -----')
-    print('----------------')
-    print('-1- Agregar Persona')
-    print('-2- Mostrar Persona')
-    print('-3- Editar Persona')
-    print('-4- Eliminar Persona')
-    print('-5- Salir')
 
-def comprobacion(msj:str)->bool:
+def comprobacionSN(msj:str)->bool:
     while True:
         entra = input(msj)
-        if entra == 'S' or entra == 's':
+        if entra == 'S' or entra == 's' or entra == 'Si' or entra == 'si':
             return True
-        elif entra == 'N' or entra == 'n':
+        elif entra == 'N' or entra == 'n' or entra == 'No' or entra == 'no':
             return False
-        else:
-            continue
+        #else:
+        #    continue
 
-# Crear
-def crear():
+
+def comprobarNum(msj:str)->int:
+    while True:
+        numero = input(msj)
+        if numero.isdigit():
+            return int(numero)
+            
+
+def menu(opcion:int,db:dict)->None:
+
+    while True:
+        print('----- CRUD -----')
+        print('----------------')
+        print('-1- Agregar Persona')
+        print('-2- Mostrar Persona')
+        print('-3- Editar Persona')
+        print('-4- Eliminar Persona')
+        print('-5- Salir')
+        opcion = input('Ingrese una opción del menu: ')
+
+        if opcion == '1':
+            db = crear(db)
+        elif opcion == '2':
+            imprimir(db)
+        elif opcion == '3':
+            db = actualizar(db)
+        elif opcion == '4':
+            db = eliminar(db)
+        elif opcion == '5':
+            break
+        else:
+            print('La opción no es valida.')
+
+# CREATE
+def crear(db:dict)->dict:
 
     dictHabilidades = {}
-
-    print()
+    
     print('Valores para la nueva persona')
 
+    msj = 'Ingrese en numero de identificación de la persona: '
+    nuip = comprobarNum(msj)
     nombre = input('Ingrese el nombre de la persona: ')
     ciudad = input('Ingrese la ciudad de residencia: ')
     profesion = input('Ingrese la profesión: ')
@@ -59,14 +85,14 @@ def crear():
     habilidades = []
     #programacion = input('Sabe de programación? [S/s] Sí [N/n] No: ')
     msj = 'Sabe de programación? [S/s] Sí [N/n] No: '
-    if comprobacion(msj) == True:
+    if comprobacionSN(msj) == True:
         dictHabilidades['programacion'] = True
     else:
         dictHabilidades['programacion'] = False
     
     #bases_de_datos = input('Sabe de bases de datos? [S/s] Sí [N/n] No: ')
     msj = 'Sabe de bases de datos? [S/s] Sí [N/n] No: '
-    if comprobacion(msj) == True:
+    if comprobacionSN(msj) == True:
         dictHabilidades['bases-de_datos'] = True
     else:
         dictHabilidades['bases-de_datos'] = False
@@ -74,7 +100,7 @@ def crear():
     while True:
         #anadirHabilidades = input('Desea añadir una habilidad más? [S/s] Sí [N/n] No: ')
         msj = 'Desea añadir una habilidad más? [S/s] Sí [N/n] No: '
-        if comprobacion(msj) == True:
+        if comprobacionSN(msj) == True:
             habilidad = input('Ingrese el nombre de la habilidad: ')
             habilidades.append(habilidad)
         else:
@@ -90,16 +116,66 @@ def crear():
         'habilidades': dictHabilidades
     }
 
-    newItem = len(directorio) + 1
-    directorio[newItem] = persona
+    # newItem = len(directorio) + 1 # Fue remplazado por nuip
+    directorio[nuip] = persona
+
+    return db
 
 
-def imprimir(db:dict):
-    consulta = int(input('A quien deseas consultar: '))
+# READ
+def imprimir(db:dict)->None:
+    msj = 'Ingrese el numero de documento de la persona a mostrar: '
+    nuip = comprobarNum(msj)
+    # Falta validar si la llave principal existe
 
-    print(f'La información de {db[consulta]["nombre"]} es')
-    print(f'Ciudad de residencia: {db[consulta]["ciudad"]}')
-    print(f'Su profesión es: {db[consulta]["profesion"]}')
-    print(f'Habilidades: {db[consulta]["habilidades"]}')
+    print(f'La información de {db[nuip]["nombre"]} es')
+    print(f'Ciudad de residencia: {db[nuip]["ciudad"]}')
+    print(f'Su profesión es: {db[nuip]["profesion"]}')
+    print(f'Habilidades: {db[nuip]["habilidades"]}')
 
-imprimir(directorio)
+
+# UPDATE
+def actualizar(db:dict)->dict:
+    msj = 'Ingrese el numero de documento de la persona a consultar: '
+    nuip = comprobarNum(msj)
+    # Falta validar si la llave principal existe
+
+    msj = 'Quiere actualizar el nombre? [S/s] Sí [N/n] No'
+    if comprobacionSN(msj):
+        nombre = input('Ingrese el nombre: ')
+        db[nuip]['nombre'] = nombre
+
+    msj = 'Quiere actualizar la ciudad? '
+    if comprobacionSN(msj):
+        ciudad = input('Ingrese la ciudad: ')
+        db[nuip].update({'ciudad': ciudad})
+
+    return db
+
+
+# DELETE
+def eliminar(db:dict)->dict:
+    msj = 'Ingrese el numero de documento de la persona a eliminar: '
+    nuip = comprobarNum(msj)
+    # Falta validar si la llave principal existe
+
+    persona = db.pop(nuip)
+
+    print(f'La iformación de {persona["nombre"]} fue eliminada.')
+
+    return db
+
+
+#dbActualizada = crear(directorio)
+#dbActualizada = crear(dbActualizada)
+#dbActualizada = crear(dbActualizada)
+#dbActualizada = crear(dbActualizada)
+#print(dbActulizada)
+
+#dbActulizada = actualizar(directorio)
+#print(dbActulizada)
+
+#dbActualizada = eliminar(directorio)
+#print(dbActualizada)
+
+menu(directorio)
