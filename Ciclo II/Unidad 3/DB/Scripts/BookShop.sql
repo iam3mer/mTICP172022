a94b15b08135415acd49379e8acedc7106b34a3b
iam3mer <jhstbarrera@utp.edu.co>
2021-08-02 13:28:55 -0500

    cambios sobre db y sql

--- Ciclo II/Unidad 3/DB/Scripts/BookShop.sql
+++ Ciclo II/Unidad 3/DB/Scripts/BookShop.sql
@@ -21,3 +21,17 @@ create table sales (
 
 -- Obtener libro por ISBN
 select * from books where isbn = 'tyrety';
+
+-- Consultar todos los libros
+SELECT * FROM books;
+
+-- Obtener existencias de un libro por id
+SELECT amount FROM stock WHERE id_book = 1;
+
+-- Realizar venta
+-- Comprobar existencias
+-- getStock()
+-- Anotar la venta
+INSERT INTO sales (sale_date,id_book,amount) VALUES (datetime('now'), 1, 5);
+-- Actualizar el stock
+UPDATE stock SET amount = 15 WHERE id_book = 1;
